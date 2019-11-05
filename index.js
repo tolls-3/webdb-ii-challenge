9bf9ffdd8fc1a77ca020a0bb149fbecf330646fe
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-11-05 20:56:22 +0100

    knex config and migrations done

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const server = require("./api/server")
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`<- Listening on port ${port} ->`);
+});
